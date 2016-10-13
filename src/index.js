1651adb3e780b32482e013ea3a82ea9250b64436
vogelino <contact@vogelino.com>
2016-10-13 14:41:30 +0200

    Added support for user and psw in url of mongo server

--- src/index.js
+++ src/index.js
@@ -5,5 +5,5 @@ import dbServer from './dbServer';
 
 const api = config.get('api');
 const database = config.get('database');
-dbServer.start(database.name, database.host, database.port);
+dbServer.start(database.name, database.host, database.port, database.user, database.psw);
 apiServer.start(api.host, api.port);
